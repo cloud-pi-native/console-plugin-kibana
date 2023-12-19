a9206646065f76f3e9d3ce25659fc6f9e6a0a871
z.karahacane <z.karahacane@actongroup.com>
2023-12-19 16:06:13 +0100

    update name logging and action

--- src/infos.ts
+++ src/infos.ts
@@ -3,7 +3,7 @@
 import type { ServiceInfos } from '@dso-console/server/src/plugins/services.js'
 
 const infos: ServiceInfos = {
-  name: 'obervability',
+  name: 'logging',
   to: () => 'https://logging.apps.c7.numerique-interieur.com',
   title: 'Observabilité',
   imgSrc: 'https://static-www.elastic.co/v3/assets/bltefdd0b53724fa2ce/blt4466841eed0bf232/5d082a5e97f2babb5af907ee/logo-kibana-32-color.svg',
