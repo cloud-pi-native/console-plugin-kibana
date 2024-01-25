6cfdea689c01d43cbbc2c7c4fb5d8546cf182112
z.karahacane <z.karahacane@actongroup.com>
2024-01-25 16:03:28 +0100

    update logo

--- src/infos.ts
+++ src/infos.ts
@@ -11,7 +11,7 @@ const infos: ServiceInfos = {
     })
     return urls
   },
-  title: 'Observabilité',
+  title: `Observabilité ${cluster.label}`,
   imgSrc: 'https://static-www.elastic.co/v3/assets/bltefdd0b53724fa2ce/blt4466841eed0bf232/5d082a5e97f2babb5af907ee/logo-kibana-32-color.svg',
   description: 'Service d\'observabilité permettant de consulter ses logs',
 }
