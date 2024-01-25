1136c93b12dbf9d4ada97c771e9714704b8533c8
z.karahacane <z.karahacane@actongroup.com>
2024-01-25 16:38:44 +0100

    update urls

--- src/infos.ts
+++ src/infos.ts
@@ -1,17 +1,18 @@
 // @ts-nocheck
 
-import type { ServiceInfos } from 'dso-console/apps/server/types/plugins/services.js'
+import type { ServiceInfos } from '@dso-console/server/types/plugins/services.js'
 
 const infos: ServiceInfos = {
   name: 'logging',
   to: ({ clusters }) => {
-    const urls = []
-    clusters.forEach(cluster => {
-      urls.push(`https://logging.apps.${cluster.label}.numerique-interieur.com`)
+    return clusters.map(cluster => {
+      return {
+        to: `https://logging.apps.${cluster.label}.numerique-interieur.com`,
+        title: `Logging ${cluster.label}`,
+      }
     })
-    return urls
   },
-  title: `Observabilité ${cluster.label}`,
+  title: `Observabilité`,
   imgSrc: 'https://static-www.elastic.co/v3/assets/bltefdd0b53724fa2ce/blt4466841eed0bf232/5d082a5e97f2babb5af907ee/logo-kibana-32-color.svg',
   description: 'Service d\'observabilité permettant de consulter ses logs',
 }
