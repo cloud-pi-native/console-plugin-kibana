e10b389138a34248babe7725182795b06a26a69e
z.karahacane <z.karahacane@actongroup.com>
2024-01-26 14:47:20 +0100

    hotfix

--- src/infos.ts
+++ src/infos.ts
@@ -5,20 +5,22 @@ import type { ServiceInfos } from '@dso-console/server/types/plugins/services.js
 const infos: ServiceInfos = {
   name: 'logging',
   to: ({ clusters }) => {
-    return getKibanaUrls({ clusters })
+    // return getKibanaUrls({ clusters })
   },
-  title: `Observabilité`,
+  title: 'Observabilité',
   imgSrc: 'https://static-www.elastic.co/v3/assets/bltefdd0b53724fa2ce/blt4466841eed0bf232/5d082a5e97f2babb5af907ee/logo-kibana-32-color.svg',
   description: 'Service d\'observabilité permettant de consulter ses logs',
 }
 
 const getKibanaUrls = ({ clusters }: ToUrlFnParamaters) => clusters.reduce((tos, cluster) => {
-  const infosArray = cluster.infos.split('\n');
-  const urlInfo = infosArray.find(infos => infos.split('=')[0] === 'url');
-  return urlInfo ? [...tos, {
-    to: `${urlInfo.split('=')[1]}`,
-    title: `Logging ${cluster.label}`,
-  }] : tos;
-}, [] as Array<{ to: string, title: string }>);
+  const infosArray = cluster.infos.split('\n')
+  const urlInfo = infosArray.find(infos => infos.split('=')[0] === 'url')
+  return urlInfo
+    ? [...tos, {
+        to: `${urlInfo.split('=')[1]}`,
+        title: `Logging ${cluster.label}`,
+      }]
+    : tos
+}, [] as Array<{ to: string, title: string }>)
 
 export default infos
