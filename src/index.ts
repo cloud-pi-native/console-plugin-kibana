e40cc1391bc8b222576e88ff687cc922e43d92c3
ArnaudTa <33383276+ArnaudTA@users.noreply.github.com>
2024-03-28 17:51:54 +0100

    feat: :sparkles: introduce idemptoency

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import { type Plugin } from '@cpn-console/hooks'
-import { createRbac, deleteRbac, updatePermission } from './functions.js'
+import { upsertProject } from './functions.js'
 import infos from './infos.js'
 
 export const plugin: Plugin = {
   infos,
   subscribedHooks: {
-    initializeEnvironment: { steps: { post: createRbac } },
-    deleteEnvironment: { steps: { main: deleteRbac } },
-    setEnvPermission: { steps: { main: updatePermission } },
+    upsertProject: {
+      steps: {
+        main: upsertProject,
+      },
+    },
+    // initializeEnvironment: { steps: { post: createRbac } },
+    // deleteEnvironment: { steps: { main: deleteRbac } },
+    // setEnvPermission: { steps: { main: updatePermission } },
   },
 }
